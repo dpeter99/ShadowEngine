562c3eeaa5cccd93fcb3fa599558518bbf49be3e
dpeter99 <dpeter99@gmail.com>
2019-03-06 14:53:42 +0100

    Build System not working

--- dependencies/dependencies.cmake
+++ dependencies/dependencies.cmake
@@ -1,37 +1,24 @@
 # sdl2 cmake project-config input for ./configure scripts
 
-set(SDL_EXEC_FOLDER dependencies/bin/)
+set(DEPENDENCIES_FOLDER ${PROJECT_SOURCE_DIR}/dependencies)
 
-set(SDL_TARGETS_FOLDER dependencies/lib/)
+include(${DEPENDENCIES_FOLDER}/SDL2/SDL2.cmake)
 
-set(SDL2_EXEC
-        ${SDL_EXEC_FOLDER}libFLAC-8.dll
-        ${SDL_EXEC_FOLDER}libfreetype-6.dll
-        ${SDL_EXEC_FOLDER}libjpeg-9.dll
-        ${SDL_EXEC_FOLDER}libmodplug-1.dll
-        ${SDL_EXEC_FOLDER}libmpg123-0.dll
-        ${SDL_EXEC_FOLDER}libogg-0.dll
-        ${SDL_EXEC_FOLDER}libpng16-16.dll
-        ${SDL_EXEC_FOLDER}libtiff-5.dll
-        ${SDL_EXEC_FOLDER}libvorbis-0.dll
-        ${SDL_EXEC_FOLDER}libvorbisfile-3.dll
-        ${SDL_EXEC_FOLDER}libwebp-7.dll
-        ${SDL_EXEC_FOLDER}SDL2.dll
-        ${SDL_EXEC_FOLDER}SDL2_gfx.dll
-        ${SDL_EXEC_FOLDER}SDL2_image.dll
-        ${SDL_EXEC_FOLDER}SDL2_mixer.dll
-        ${SDL_EXEC_FOLDER}SDL2_net.dll
-        ${SDL_EXEC_FOLDER}SDL2_ttf.dll
-        ${SDL_EXEC_FOLDER}zlib1.dll
+
+
+set(DEPENDENCIES_EXEC
+        ${SDL2_EXEC}
+        ${DEPENDENCIES_FOLDER}/SDL2/bin/SDL2.dll
         )
 
-set(SDL2_TARGETS
-        ${SDL_TARGETS_FOLDER}libSDL2main.a
-        ${SDL_TARGETS_FOLDER}libSDL2.dll.a
-        ${SDL_TARGETS_FOLDER}libSDL2_gfx.a
-        ${SDL_TARGETS_FOLDER}libSDL2_image.dll.a
-        ${SDL_TARGETS_FOLDER}libSDL2_mixer.dll.a
-        ${SDL_TARGETS_FOLDER}libSDL2_net.dll.a
-        ${SDL_TARGETS_FOLDER}libSDL2_ttf.dll.a
-        ${SDL_TARGETS_FOLDER}libSDL2_test.a
+set(DEPENDENCIES_TARGETS
+        ${SDL2_TARGETS}
+        ${DEPENDENCIES_FOLDER}/SDL2/lib/libSDL2.dll.a
+        ${DEPENDENCIES_FOLDER}/SDL2/lib/libSDL2_test.a
+        ${DEPENDENCIES_FOLDER}/SDL2/lib/libSDL2main.a
         )
+
+set(DEPENDENCIES_INCLUDES
+        ${SDL2_INCLUDES}
+        )
+
