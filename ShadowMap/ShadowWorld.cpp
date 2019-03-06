3f2984e5c3829ffc3ad40d1a79cbcba08178796a
dpeter99 <dpeter99@gmail.com>
2019-03-06 15:10:27 +0100

    ShadowWorld

---
+++ ShadowMap/ShadowWorld.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dpete on 3/6/2019.
+//
+
+#include "ShadowWorld.h"
---
+++ ShadowMap/ShadowWorld.h
@@ -0,0 +1,16 @@
+//
+// Created by dpete on 3/6/2019.
+//
+
+#ifndef SHADOWENGINE_SHADOWWORLD_H
+#define SHADOWENGINE_SHADOWWORLD_H
+
+
+//Holds and manages the maps
+//Loads in the sorrounding ones, loads out the old ones
+class ShadowWorld {
+
+};
+
+
+#endif //SHADOWENGINE_SHADOWWORLD_H
