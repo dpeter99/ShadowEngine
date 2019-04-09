c298cfebc2049da9fc1afd31001fb1e2d2afa449
dpeter99 <dpeter99@gmail.com>
2019-04-09 15:05:32 +0200

    New Bindings

--- Planning/src/Input.cpp
+++ Planning/src/Input.cpp
@@ -1,131 +1,120 @@
-//#ifdef INPUT
-
 #include <string>
-#include <iostream>
 using namespace std;
 
-namespace Planning {
-	class Binding;
+class Binding;
 
-	template<class T>
-	class InputAction {
-	public:
-		InputAction(std::string a,const Binding& b, bool continous = false);
-	};
+template<class T>
+class InputAction {
+public:
+	InputAction(std::string a, Binding* b);
+};
 
-	class Vector2float;
-	class Modifier;
+class Vector2float;
+class Modifier;
 
-	class Binding
-	{
-	public:
-		Binding& AddModifier(const Modifier& b);
-		~Binding()
-		{
-			std::cout << "Binding free";
-		}
-	};
+class Binding
+{
+public:
+	Binding* AddModifier(const ::Modifier* b);
+};
 
-	class Binding1D : public Binding
-	{
-	public:
-		Binding1D(const Binding& keyboard_binding, const Binding& keyboard_binding1);
-	};
+class Binding1D : public Binding
+{
+public:
+	Binding1D(Binding* _xpositive, Binding* _xnegative);
+};
 
-	class Binding2D : public Binding
-	{
-	public:
-		Binding2D(const Binding& _xpositive, const Binding& _xnegative,
-			const Binding& _ypositive, const Binding& _ynegative);
-	};
+class Binding2D : public Binding
+{
+public:
+	Binding2D(const ::Binding* _xpositive, const ::Binding* _xnegative,
+		const ::Binding* _ypositive, const ::Binding* _ynegative);
+};
 
-	class BindingOR : public Binding
-	{
-	public:
-		BindingOR(const Binding& _a, const Binding& _b);
-	};
+class BindingOR : public Binding
+{
+public:
+	BindingOR(const ::Binding* _a, const ::Binding* _b);
+};
 
-	class BindingAND : public Binding
-	{
-	public:
-		BindingAND(const Binding& _a, const Binding& _b);
-	};
+class BindingAND : public Binding
+{
+public:
+	BindingAND(const Binding& _a, const Binding& _b);
+};
 
-	class KeyboardBinding : public Binding {
-	public:
-		KeyboardBinding(const char* str);
-	};
+class KeyboardBinding : public Binding {
+public:
+	KeyboardBinding(const char* str);
+};
 
-	class InputMapBinding : public Binding
-	{
-	public:
-		InputMapBinding(const char* map);
-	};
-
-	class Modifier
-	{
-
-	};
-
-	class ModifierHold : public Modifier
-	{
-	public:
-		ModifierHold(float time, bool continous = false);
-	};
-
-	class ModifierRepeat : public Modifier
-	{
-	public:
-		ModifierRepeat(int count);
-	};
+class InputMapBinding : public Binding
+{
+public:
+	InputMapBinding(const char* map);
+};
 
 
-	void main2() {
+class Modifier
+{
 
-		new InputAction<float>("Run", Binding1D(KeyboardBinding("W"), KeyboardBinding("S")));
+};
 
-		new InputAction<bool>("Fire", KeyboardBinding("Space"));
+class ModifierHold : public Modifier
+{
+public:
+	ModifierHold(float time);
+};
 
-		new InputAction<bool>("Charge",
-			KeyboardBinding("Q").AddModifier(ModifierHold(0.3))
-			);
-
-		new InputAction<Vector2float>("Direction",
-			Binding2D(
-				KeyboardBinding("W"),
-				KeyboardBinding("S"),
-				KeyboardBinding("A"),
-				KeyboardBinding("D")
-			), true);
+class ModifierRepeat : public Modifier
+{
+public:
+	ModifierRepeat(int count);
+};
 
 
-		new InputAction<bool>("QuickTime",
-			KeyboardBinding("Q").AddModifier(ModifierRepeat(5)));
+void main() {
 
-		new InputAction<bool>("Cause", BindingOR(
-			KeyboardBinding("R"),
-			KeyboardBinding("J")
+	new InputAction<float>("Run", new Binding1D(new KeyboardBinding("W"), new KeyboardBinding("S")));
+
+	new InputAction<bool>("Fire", new KeyboardBinding("Space"));
+
+	new InputAction<bool>("Charge",
+		(new KeyboardBinding("Q"))->AddModifier(new ModifierHold(0.3))
+		);
+
+	new InputAction<Vector2float>("Direction",
+		new Binding2D(
+			new KeyboardBinding("W"),
+			new KeyboardBinding("S"),
+			new KeyboardBinding("A"),
+			new KeyboardBinding("D")
 		));
 
-		new InputAction<float>("Uhhh", BindingOR(
-			Binding1D(
-				KeyboardBinding("Q"),
-				KeyboardBinding("E")
-			),
-			Binding1D(
-				KeyboardBinding("+"),
-				KeyboardBinding("-")
-			)
-		));
 
-		new InputAction<bool>("Combo", BindingAND(
-			InputMapBinding("F"),
-			InputMapBinding("G")
-		).AddModifier(ModifierHold(0.5, true)
-		), true);
-	}
+	new InputAction<bool>("QuickTime",
+		(new KeyboardBinding("Q"))->AddModifier(new ModifierRepeat(5)));
+
+	new InputAction<bool>("Cause", new BindingOR(
+		new KeyboardBinding("R"),
+		new KeyboardBinding("J")
+	));
+
+	new InputAction<float>("Uhhh", new BindingOR(
+		new Binding1D(
+			new KeyboardBinding("Q"),
+			new KeyboardBinding("E")
+		),
+		new Binding1D(
+			new KeyboardBinding("+"),
+			new KeyboardBinding("-")
+		)
+	));
 
 
+	new InputAction<bool>("Combo", BindingAND(
+		InputMapBinding("F"),
+		InputMapBinding("G")
+	).AddModifier(ModifierHold(0.5, true)
+	), true);
 }
-
-#endif
