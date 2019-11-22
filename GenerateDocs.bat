c253e5e3fc94be6ecfaea749b5313adc7e3cd1fb
dpeter99 <dpeter99@gmail.com>
2019-11-22 12:14:22 +0100

    Documentation Gen

---
+++ GenerateDocs.bat
@@ -0,0 +1,3 @@
+P:\csharp\_Other\code2yaml\target\Release\code2yaml\net462\code2yaml.exe  ShadowEngine\docs\code2yaml.json
+
+packages\docfx.console.2.47.0\tools\docfx.exe ShadowEngine\docs\docfx.json
