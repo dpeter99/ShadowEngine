51f8495798375a1b2da1c23bc5c9bc25accfb92e
dpeter99 <dpeter99@gmail.com>
2020-01-05 18:14:03 +0100

    Build file echo

--- docs-source/build-docs.cmd
+++ docs-source/build-docs.cmd
@@ -1,3 +1,6 @@
-//..\dependencies\code2yaml\code2yaml.exe code2yaml.json
+echo Building Documentation
+echo %cd%
+
+..\dependencies\code2yaml\code2yaml.exe code2yaml.json
 
 ..\packages\docfx.console.2.48.0\tools\docfx.exe docfx.json
