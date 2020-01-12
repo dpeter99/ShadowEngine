850ea7fcb28ecb124f55fe913daf2835117bad96
dpeter99 <dpeter99@gmail.com>
2020-01-12 13:13:11 +0100

    [CLI] git path fix

--- .github/actions/publish-to-ghpages/publish.cmd
+++ .github/actions/publish-to-ghpages/publish.cmd
@@ -9,7 +9,7 @@ dir
 echo %cd%
 
 git init
-git remote add origin "https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY"
+git remote add origin "https://x-access-token:%GITHUB_TOKEN%github.com/%GITHUB_REPOSITORY%"
 git add .
 git commit -m "ApiGen Code Docs Regenerated"
 git push origin "master:${PUSH_TO_BRANCH}" -f
