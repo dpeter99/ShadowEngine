b691c88705941bcc256b741fcd62bf54efa67ab1
dpeter99 <dpeter99@gmail.com>
2020-01-12 13:21:30 +0100

    [CLI] name and email

--- .github/actions/publish-to-ghpages/publish.cmd
+++ .github/actions/publish-to-ghpages/publish.cmd
@@ -8,6 +8,9 @@ cd %FOLDER_TO_PUSH%
 dir
 echo %cd%
 
+git config --global user.email "dpeter99@gmail.com"
+git config --global user.name "Doc gen"
+
 git init
 git remote add origin "https://x-access-token:%GITHUB_TOKEN%github.com/%GITHUB_REPOSITORY%"
 git add .
