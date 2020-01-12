3152928f0bc9d88866cc7be962255ad5dd280c89
dpeter99 <dpeter99@gmail.com>
2020-01-12 13:26:01 +0100

    [CLI] CMD fix

--- .github/actions/publish-to-ghpages/publish.cmd
+++ .github/actions/publish-to-ghpages/publish.cmd
@@ -12,7 +12,7 @@ git config --global user.email "dpeter99@gmail.com"
 git config --global user.name "Doc gen"
 
 git init
-git remote add origin "https://x-access-token:%GITHUB_TOKEN%github.com/%GITHUB_REPOSITORY%"
+git remote add origin "https://x-access-token:%GITHUB_TOKEN%@github.com/%GITHUB_REPOSITORY%"
 git add .
 git commit -m "ApiGen Code Docs Regenerated"
 git push origin "master:${PUSH_TO_BRANCH}" -f
