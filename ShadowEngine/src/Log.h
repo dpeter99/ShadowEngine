77dc9d23ed2f7f1fecbb682a661593d151677db0
dpeter99 <dpeter99@gmail.com>
2020-10-11 12:50:37 +0200

    Fix spdlog

--- ShadowEngine/src/Log.h
+++ ShadowEngine/src/Log.h
@@ -3,9 +3,12 @@
 #include "ShadowModules/ShadowModule.h"
 #include "Core/Core.h"
 
-
+// This ignores all warnings raised inside External headers
+#pragma warning(push, 0)
 #include "spdlog/spdlog.h"
 #include "spdlog/fmt/ostr.h"
+#pragma warning(pop)
+
 
 namespace ShadowEngine {
 
